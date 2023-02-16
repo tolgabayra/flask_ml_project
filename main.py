2264e667e89a0bd202e2c3f98fd82d320af663bc
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-02-16 19:59:03 +0300

    config file added

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from controller.user_controller import user_controller
+from config import Config
+
 db = SQLAlchemy()
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.register_blueprint(user_controller, url_prefix='/api/v1')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:root@localhost:5432'
+    app.config.from_object(config_class)
     app.run(port=5000)
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
+
+    app.register_blueprint(user_controller, url_prefix='/api/v1')
+
+
     return app
 
 
